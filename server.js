358bde09451200b9370e2036257e295ad6ee77bc
m4rc0s <marcos.araujo@dextra-sw.com>
2020-06-05 16:12:01 -0300

    Change port and add path

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
-
+const path = require('path')
 const app = express();
+
+const PORT = process.env.PORT || 5000
+
 
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -9,4 +12,4 @@ app.get('/', (req, res) => {
     res.send('Einstein bot app loaded.')
 })
 
-app.listen(3000, () => console.log('Server running.'))
+app.listen(PORT, () => console.log('Server running.'))
