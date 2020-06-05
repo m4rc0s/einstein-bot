fc67b3c6b2f55b998be4715dfb2011e1e719862f
m4rc0s <marcos.araujo@dextra-sw.com>
2020-06-05 16:08:17 -0300

    Heroku app confioguration

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express')
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')))
+
 
 app.get('/', (req, res) => {
     res.send('Einstein bot app loaded.')
